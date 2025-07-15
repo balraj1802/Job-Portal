6dc7ac3ce7a9efbb4d5ac548e0f77379bd334b74
balraj1802 <codekarbalraj@gmail.com>
2025-07-15 21:44:23 +0000

    CategoryCarousel

--- frontend/src/components/ui/radio-group.jsx
+++ frontend/src/components/ui/radio-group.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
 import { Circle } from "lucide-react"
@@ -13,7 +11,7 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 
 const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
   return (
-    <RadioGroupPrimitive.Item
+    (<RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
         "aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
@@ -23,7 +21,7 @@ const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
         <Circle className="h-2.5 w-2.5 fill-current text-current" />
       </RadioGroupPrimitive.Indicator>
-    </RadioGroupPrimitive.Item>
+    </RadioGroupPrimitive.Item>)
   );
 })
 RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
