5ccdaeea8da1d494c4431519efc4b0c45560b3ca
balraj1802 <codekarbalraj@gmail.com>
2025-07-16 11:12:08 +0000

    Footer

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import Navbar from '../../../jobportal-yt/frontend/src/components/shared/Navbar'
+import Navbar from './shared/Navbar';
 import FilterCard from './FilterCard'
 import Job from './Job';
 import { useSelector } from 'react-redux';
 import { motion } from 'framer-motion';
+
 
 // const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
