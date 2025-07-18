7b900d02286e77c1893e4f2264afeb4d4102de0a
balraj1802 <codekarbalraj@gmail.com>
2025-07-18 22:51:13 +0000

    changes_in_directories

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:[
+    "http://localhost:5173",
+    "https://fictional-space-spoon-7vr9x6wjx9prhwpp7-5173.app.github.dev"
+  ],
     credentials:true
 }
 
