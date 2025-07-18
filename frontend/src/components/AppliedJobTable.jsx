2b621db6f0d0213176fa269df2f24f71cdf76510
balraj1802 <codekarbalraj@gmail.com>
2025-07-18 22:57:56 +0000

    files_for_admin

--- frontend/src/components/AppliedJobTable.jsx
+++ frontend/src/components/AppliedJobTable.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
-import { Badge } from './ui/badge'
+import { Badge } from './ui/badge';
 import { useSelector } from 'react-redux'
+
 
 const AppliedJobTable = () => {
     const {allAppliedJobs} = useSelector(store=>store.job);
